8569e2fcd0e0dfb19b62c3c10a6de6170a89b9ab
Jhonanttan Willyan <jhonanttan1@gmail.com>
2023-02-14 22:12:57 -0300

    Nova atualização

--- src/Modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/Modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,23 @@
 import csvParse from "csv-parse";
 import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implementations/categoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase {
 
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+        const categories: IImportCategory[] = []
 
         const parseFile = csvParse();
 
@@ -12,7 +25,36 @@ class ImportCategoryUseCase {
         stream.pipe(parseFile);
 
         parseFile.on("data", async (line) => {
-            console.log(line)
+            const [name, description] = line
+            categories.push({
+                name,
+                description,
+            })
+        })
+        .on("end", () => {
+            resolve(categories);
+        })
+        .on("error", (err) => {
+            reject(err)
+        })
+        })
+    }
+    async execute(file: Express.Multer.File): Promise <void> {
+        const categories = await this.loadCategories(file)
+        
+        categories.map(async category => {
+            const { name, description } = category;
+
+            const existsCategory = this.categoriesRepository.findByName(name);
+
+            if (!existsCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description,
+                })
+            }
+
+
         })
     }
 }
